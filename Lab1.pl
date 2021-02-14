ea5686cc81377adb2e5ac7c72b90dfd5ad7f1e36
Ponchu24 <panh100@gmail.com>
2021-02-15 02:03:45 +0300

    #7

--- Lab1.pl
+++ Lab1.pl
@@ -69,3 +69,7 @@ brothers(X):-brother(Y,X),write(Y),nl,fail.
 
 husband(X,Y):-parent(X,Z),mother(Y,Z),X\=Y.
 husband(X):-husband(Y,X),write(Y),nl.
+
+b_s(X,Y):-mother(Z,X),mother(Z,Y),X\=Y.
+b_s(X):-b_s(Y,X),write(Y),nl,fail.
+
